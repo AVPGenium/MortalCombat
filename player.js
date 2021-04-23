b84ea46162b25b82dfcb9a332ba89f3d254df5fa
Andrei Poliakov <avpgenium@yandex.ru>
2021-04-24 00:44:42 +0300

    homework 8: use classes

--- player.js
+++ player.js
@@ -1,43 +1,45 @@
-function changeHp(value) {
-    this.hp -= value;
-    if (this.hp <= 0) {
-        this.hp = 0;
+class Player {
+    constructor(props) {
+        this.player = props.player;
+        this.name = props.name;
+        this.img = props.img;
+        this.hp = props.hp;
+        this.weapon = props.weapon;
+    }
+
+    changeHp = (value) => {
+        this.hp -= value;
+        if (this.hp <= 0) {
+            this.hp = 0;
+        }
+    }
+
+    elHp = () => {
+        return document.querySelector('.player' + this.player + ' .life');
+    }
+
+    renderHp = () => {
+        const $playerLife = this.elHp();
+        $playerLife.style.width = this.hp + '%';
+    }
+
+    attack = () => {
+        console.log(this.name + ' Fight...')
     }
 }
 
-function elHp() {
-    return document.querySelector('.player' + this.player + ' .life');
-}
-
-function renderHp() {
-    const $playerLife = this.elHp();
-    $playerLife.style.width = this.hp + '%';
-}
-
-export let player1 = {
+export let player1 = new Player({
     player: 1,
     name: "player1",
     img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
     hp: 100,
-    weapon: ["weapon1, weapon2"],
-    changeHp,
-    elHp,
-    renderHp,
-    attack: function () {
-        console.log(this.name + ' Fight...')
-    }
-};
+    weapon: ["weapon1, weapon2"]
+});
 
-export let player2 = {
+export let player2 = new Player({
     player: 2,
     name: "player2",
     img: "http://reactmarathon-api.herokuapp.com/assets/kitana.gif",
     hp: 100,
-    weapon: ["weapon1, weapon2"],
-    changeHp,
-    elHp,
-    renderHp,
-    attack: function () {
-        console.log(this.name + ' Fight...')
-    }
-};
+    weapon: ["weapon1, weapon2"]
+});

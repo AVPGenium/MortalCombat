5b956ce96cc72ba07a78ef60c88914938772cbc1
Andrei Poliakov <avpgenium@yandex.ru>
2021-04-29 00:21:33 +0300

    homework 9: fix players loading

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ import Game from './Game';
 import Player from "./Player";
 
 const player1 = new Player(JSON.parse(localStorage.getItem('player1')));
-const player2 = fetch('https://reactmarathon-api.herokuapp.com/api/mk/player/choose')
-    .then(res => res.json());
+const player2 = new Player(
+    fetch('https://reactmarathon-api.herokuapp.com/api/mk/player/choose')
+        .then(res => res.json())
+);
 
-const game = new Game(player1. player2);
+const game = new Game(player1, player2, 'arenas');
 game.start();
